28dfedb46c216df6c30fe890c53683d15e9a820b
Adam Szymanowski <adam.szym@yahoo.pl>
2022-09-13 14:34:03 +0200

    Implement and test multiplication helpers.

--- book/Roughgarden/multiplication.py
+++ book/Roughgarden/multiplication.py
@@ -1,2 +1,17 @@
-def output_two():
-    return 2
+import math
+
+def number_of_digits(n):
+    return math.floor(math.log10(n)) + 1
+
+def is_power_of_2(n):
+    return (math.log(n, 2) % 1) == 0
+
+def halves(i):
+    digits = number_of_digits(i)
+    if not is_power_of_2(digits):
+        # this is an assumption error
+        pass
+
+    half_1, half_2 = ((i // 10), (i % 10)) # assume it's a 2-digit number for now
+    return (half_1, half_2)
+
--- tests/Roughgarden/test_multiplication.py
+++ tests/Roughgarden/test_multiplication.py
@@ -1,7 +1,49 @@
 import unittest
 from book.Roughgarden.multiplication import *
 
-class TestsForMultiplication(unittest.TestCase):
+class TestsForNumberOfDigits(unittest.TestCase):
 
-    def test_output_two(self):
-        self.assertEqual(output_two(), 2)
+    def test_2_digits(self):
+        self.assertEqual(number_of_digits(21), 2)
+
+    def test_3_digits(self):
+        self.assertEqual(number_of_digits(321), 3)
+
+    def test_4_digits(self):
+        self.assertEqual(number_of_digits(4321), 4)
+
+    def test_5_digits(self):
+        self.assertEqual(number_of_digits(54321), 5)
+
+
+class TestsForIsPowerOf2(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(is_power_of_2(1))
+    
+    def test2(self):
+        self.assertTrue(is_power_of_2(2))
+
+    def test3(self):
+        self.assertFalse(is_power_of_2(3))
+
+    def test4(self):
+        self.assertTrue(is_power_of_2(4))
+
+    def test5(self):
+        self.assertFalse(is_power_of_2(5))
+
+    def test8(self):
+        self.assertTrue(is_power_of_2(8))
+
+    def test16(self):
+        self.assertTrue(is_power_of_2(16))
+
+
+class TestsForHalves(unittest.TestCase):
+
+    def test2(self):
+        self.assertEqual(halves(78), (7, 8))
+
+    def test4(self):
+        self.assertEqual(halves(1122), (11, 22))
