87ebef1959cfe39d79d7c89c25f435daf422feb4
Adam Szymanowski <adam.szym@yahoo.pl>
2022-09-13 21:52:57 +0200

    Complete Chapter 1.3 Karatsuba Multiplication. Simplify test suite.

--- book/Roughgarden/multiplication.py
+++ book/Roughgarden/multiplication.py
@@ -1,17 +1,93 @@
 import math
 
+# NOTE: conditions are checks in algorithm implementation NOT in helper functions themselves
+# (aside from obvious error check) for clarity and ease of implementation
+
+# helper functions
 def number_of_digits(n):
-    return math.floor(math.log10(n)) + 1
+    if n == 0:
+        return 1
+    else:
+        return math.floor(math.log10(n)) + 1
 
 def is_power_of_2(n):
     return (math.log(n, 2) % 1) == 0
 
 def halves(i):
     digits = number_of_digits(i)
-    if not is_power_of_2(digits):
-        # this is an assumption error
-        pass
 
-    half_1, half_2 = ((i // 10), (i % 10)) # assume it's a 2-digit number for now
+    half_1, half_2 = ( (i // (10**(digits//2))), (i % 10**(digits//2)) )
     return (half_1, half_2)
 
+def error_check_and_get_number_of_digits(x, y):
+    if x < 0 or y < 0:
+        raise InputError(f"x, y must be positive, NOT (x={x}, y={y})!")
+    
+    x_digits, y_digits = number_of_digits(x), number_of_digits(y)
+    # error checks
+    if not x_digits == y_digits:
+        raise InputError(f"x,y must have the same number of digits, NOT ({x_digits}, {y_digits})!")
+    
+    if not is_power_of_2(x_digits):
+        raise AssumptionError(f"x must be a n-digit integer, where n is power of 2, NOT (n={x_digits})")
+    
+    return x_digits
+
+# exceptions
+class InputError(Exception):
+    """ Input: two n-digit positive integers"""
+    
+class AssumptionError(Exception):
+    """Assumption: n is power of 2 """
+    
+# algorithms
+def recursive_integer_multiplication(x, y):
+    """
+    Input:      two n-digit positive integers
+    Output:     the product x*y
+    Assumption: n is power of 2
+    """
+    # error checks
+    n = error_check_and_get_number_of_digits(x, y)
+    
+    # implementation
+    if n == 1:
+        return x * y
+    else:
+        a, b = halves(x)
+        c, d = halves(y)
+        
+        ac, ad, bc, bd = (
+            recursive_integer_multiplication(a,c), 
+            recursive_integer_multiplication(a,d), 
+            recursive_integer_multiplication(b,c), 
+            recursive_integer_multiplication(b,d)
+        )
+        return (10**n * ac) + (10**(n//2) * (ad + bc) + bd)
+    
+    
+def karatsuba(x, y):
+    """
+    Input:      two n-digit positive integers
+    Output:     the product x*y
+    Assumption: n is power of 2
+    """
+    # error checks
+    n = error_check_and_get_number_of_digits(x, y)
+    
+    # implementation
+    if n == 1:
+        return x * y
+    else:
+        a, b = halves(x)
+        c, d = halves(y)
+
+        p, q = (a+b), (c+d)
+        ac, bd = (
+            karatsuba(a, c),
+            karatsuba(b, d)
+        )
+        adbc = (p*q) - ac - bd
+    
+    return (10**n * ac) + (10**(n//2) * adbc + bd)
+    
--- tests/Roughgarden/test_multiplication.py
+++ tests/Roughgarden/test_multiplication.py
@@ -1,49 +1,57 @@
 import unittest
 from book.Roughgarden.multiplication import *
 
+# helpers
 class TestsForNumberOfDigits(unittest.TestCase):
-
-    def test_2_digits(self):
-        self.assertEqual(number_of_digits(21), 2)
-
-    def test_3_digits(self):
+    def test_cases(self):
+        self.assertEqual(number_of_digits(0), 1)
+        self.assertEqual(number_of_digits(20), 2)
         self.assertEqual(number_of_digits(321), 3)
-
-    def test_4_digits(self):
         self.assertEqual(number_of_digits(4321), 4)
-
-    def test_5_digits(self):
         self.assertEqual(number_of_digits(54321), 5)
 
 
 class TestsForIsPowerOf2(unittest.TestCase):
-
-    def test1(self):
+    def test_true(self):
         self.assertTrue(is_power_of_2(1))
-    
-    def test2(self):
         self.assertTrue(is_power_of_2(2))
-
-    def test3(self):
-        self.assertFalse(is_power_of_2(3))
-
-    def test4(self):
         self.assertTrue(is_power_of_2(4))
-
-    def test5(self):
-        self.assertFalse(is_power_of_2(5))
-
-    def test8(self):
         self.assertTrue(is_power_of_2(8))
-
-    def test16(self):
         self.assertTrue(is_power_of_2(16))
+        
+    def test_false(self):
+        self.assertFalse(is_power_of_2(3))
+        self.assertFalse(is_power_of_2(5))
 
 
 class TestsForHalves(unittest.TestCase):
-
-    def test2(self):
+    def test_cases(self):
         self.assertEqual(halves(78), (7, 8))
-
-    def test4(self):
         self.assertEqual(halves(1122), (11, 22))
+        self.assertEqual(halves(10012002), (1001, 2002))
+        self.assertEqual(halves(1234_1234_5678_5678), (1234_1234, 5678_5678))
+
+
+class TestsForErrorCheckAndGetNumberOfDigits(unittest.TestCase):
+    def test_input_error(self):
+        self.assertRaises(InputError, error_check_and_get_number_of_digits, 321, 21)
+        self.assertRaises(InputError, error_check_and_get_number_of_digits, -321, 321)
+        self.assertRaises(InputError, error_check_and_get_number_of_digits, 107, -767)
+        self.assertRaises(InputError, error_check_and_get_number_of_digits, -555, -767)
+        
+    def test_assumption_error(self):
+        self.assertRaises(AssumptionError, error_check_and_get_number_of_digits, 787, 101)
+        
+# algorithms
+class TestsForRecursiveIntegerMultiplication(unittest.TestCase):
+    def test_cases(self):
+        self.assertEqual(recursive_integer_multiplication(5,5), 5*5)
+        self.assertEqual(recursive_integer_multiplication(20, 20), 20*20)
+        self.assertEqual(recursive_integer_multiplication(3418, 1024), 3418*1024)
+
+        
+class TestsForKaratsuba(unittest.TestCase):
+    def test_cases(self):
+        self.assertEqual(karatsuba(20, 20), 20*20)
+        self.assertEqual(karatsuba(2048, 4096), 2048*4096)
+        self.assertEqual(karatsuba(8765_4321, 1234_5678), 8765_4321*1234_5678)
